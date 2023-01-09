67146fbdb422046b5b063c7154242c00d23348bd
berndreinhold <hold-und-rein@web.de>
2023-01-09 01:10:25 +0100

    doc(heat_scatter.py): proper docstring for paired_corr_plot() function. Function was renamed.

--- examples/corr_plot.py
+++ examples/corr_plot.py
@@ -14,13 +14,38 @@ sns.set_theme(style="whitegrid")
 
 
 
-def corr_plot(df : pd.DataFrame, category_col : str = None, corr_columns : list = None, **kwargs):
-    
+def paired_corr_plot(df : pd.DataFrame, category_col : str = None, corr_columns : list = None, **kwargs):
+    """
+    plot paired correlation matrices of a dataframe in a heatmap
+    paired means one dataframe selected for one value of the category_col is shown in the upper triangle and the other in the lower triangle
+    different cases are distinguished by the number of distinct values of the category_col:
+    Right now, only 2 or 3 categories are supported, Otherwise the correlation matrix of the whole dataframe is plotted
+    in case of 2 category values, there is one paired correlation matrix, in case of 3 category values there are two (subplots = category values - 1)
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        dataframe to plot and that contains also the category column
+        Right now only support for one category column is implemented.
+        Support for more than one is conceivable though.
+
+    category_col : str
+        name of the column that contains the category values to distinguish
+
+    corr_columns : list
+        list of column names to calculate the correlation matrix from
+
+    **kwargs : dict
+        keyword arguments passed to the seaborn.heatmap function
+    """
+
     # determine the distinct values of category_col:
     if category_col is None:
         categories = []
     else:
         categories = df[category_col].unique().tolist()
+    
+    
     if len(categories)<1 or len(categories)==1 or len(categories)>3:
         fig, ax = plt.subplots(figsize=(9.5,6))
 
@@ -122,7 +147,7 @@ df = df.dropna(axis=0)
 
 category_col = "species"  # three species: two paired correlation plots
 #category_col = "sex"  # two categories: Female, Male: one paired correlation plot
-fig, ax = corr_plot(df, category_col=category_col, annot=True, fmt=".2f", cmap="YlGnBu")
+fig, ax = paired_corr_plot(df, category_col=category_col, annot=True, fmt=".2f", cmap="YlGnBu")
 plt.show()
 
 #sns.pairplot(df, hue="species", vars=columns_, diag_kind="kde", kind="reg", plot_kws={'line_kws':{'color':'red'}, 'scatter_kws': {'alpha': 0.3}})
